39dd4a45e14671a1acae507cd8ac9942e217ba36
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-06 23:56:38 +0600

    profile signal

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,3 +12,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+
+@receiver(post_save, sender=User)
+def post_save_create_profile(sender, instance, created, **kwargs):
+    print(sender)
+    print(instance)
+    print(created)
+    if created:
+        Profile.objects.create(user=instance)
