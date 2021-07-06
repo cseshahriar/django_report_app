463c237616e52a871d3d1bc942450a85c9ded520
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-06 23:43:18 +0600

    static and media settings

--- reports_project/settings.py
+++ reports_project/settings.py
@@ -125,11 +125,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+# static settings
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
