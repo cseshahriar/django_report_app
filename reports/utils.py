07a5c891118ab08363ae56fb571ab55711dab2ae
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-09 21:56:47 +0600

    base64 to image

--- reports/utils.py
+++ reports/utils.py
@@ -2,6 +2,7 @@ import base64, uuid
 from django.core.files.base import ContentFile
 
 def get_report_image(data):
+    """ base64 data to image conversion """
     _ , str_image = data.split(';base64')
     decoded_img = base64.b64decode(str_image)
     img_name = str(uuid.uuid4())[:10] + '.png'
